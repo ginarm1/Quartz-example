01c13849b9661f45843f94daaff3e2a14cb09dce
g.armonaitis <g.armonaitis@PCGINARML1>
2022-10-20 13:57:26 +0300

    First commit Quartz console example

---
+++ QuartzHostedService/HelloWorldJob2.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace QuartzHostedServices
+{
+    public class HelloWorldJob2 : IJob
+    {
+        private readonly ILogger<HelloWorldJob2> _logger;
+
+        public HelloWorldJob2(ILogger<HelloWorldJob2> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Hello world! V2");
+            return Task.CompletedTask;
+        }
+    }
+}
